80cc6a642c7b2365b17de6e132fa93ab0e79a890
furkan <ulutasfurkan@gmail.com>
2022-07-15 21:34:55 +0300

    ExerciseDetail and Detail component improvements

---
+++ src/components/Detail.js
@@ -0,0 +1,54 @@
+import React from 'react'
+import { Typography, Stack, Button } from '@mui/material'
+
+import BodyPartImage from '../assets/icons/body-part.png'
+import TargetImage from '../assets/icons/target.png'
+import EquipmentImage from '../assets/icons/body-part.png'
+
+
+const Detail = ({ exerciseDetail }) => {
+  const { bodyPart, gifUrl, name, target, equipment } = exerciseDetail
+
+  const extraDetail = [
+    {
+        icon : BodyPartImage,
+        name : bodyPart,
+    },
+    {
+        icon : TargetImage,
+        name : target,
+    },
+    {
+        icon : EquipmentImage,
+        name : equipment,
+    },
+  ]
+
+  return (
+    <Stack gap='60px' sx={{ flexDirection : { lg : 'row',}, p : '20px', alignItems : 'center'}}>
+        <img src={gifUrl} alt={name} loading='lazy' className='detail-image' />
+        <Stack sx={{ gap : { lg: '35px', xs : '20px'}}}>
+            <Typography>
+                {name}
+            </Typography>
+            <Typography variant='h6'>
+                Exercises keep you strong. {name} {` `}
+                is one of the best exercises to target you {target}. It will help you  
+                improve your mood and gain energy.
+            </Typography>
+            {extraDetail.map((item) => (
+                <Stack key={item.name} direction='row' gap='24px' alignItems='center'>
+                    <Button sx={{ background : 'black', borderRadius : '50%', width : '100px', height : '100px'}}>
+                        <img src={item.icon} alt={bodyPart} loading='lazy' style={{ width : '50px', height : '50px'}} />
+                    </Button>
+                    <Typography textTransform='capitalize' variant='h6'>
+                        {item.name}
+                    </Typography>   
+                </Stack>
+            ))}
+        </Stack>
+    </Stack>
+  )
+}
+
+export default Detail
---
+++ src/components/ExerciseVideos.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const ExerciseVideos = () => {
+  return (
+    <div>ExerciseVideo</div>
+  )
+}
+
+export default ExerciseVideos
